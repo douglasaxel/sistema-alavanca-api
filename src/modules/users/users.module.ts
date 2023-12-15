75ea9a9cd7ed1345843766f8e6e3eec56363e992
Douglas Axel <douglasaaxelkjellin@gmail.com>
2023-12-15 16:52:29 -0300

    FIX: import modulo colaborador

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { CollaboratorsService } from '../collaborators/collaborators.service';
 
 @Module({
 	controllers: [UsersController],
-	providers: [UsersService],
+	providers: [UsersService, CollaboratorsService],
 })
 export class UsersModule {}
